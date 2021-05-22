7c6bdf1ab675595dd3b8ccc998865b04b74f20bd
BuildTools <unconfigured@null.spigotmc.org>
2021-05-22 16:02:53 +0200

    feat(array) implemented array and itteration function

---
+++ Iterator.js
@@ -0,0 +1,25 @@
+var Storeitems = [{
+    name: "Mario Kart",
+    price: "45.99"
+}, {
+    name: "Mario Party",
+    price: "59.99"
+}, {
+    name: "Legend of Zelda",
+    price: "55.49"
+}, {
+    name: "Luigi's Mansion",
+    price: "65.99"
+}, {
+    name: "Wii Sports",
+    price: "69.99"
+}, {
+    name: "Switch the game",
+    price: "34.99"
+}, {
+    name: "Mario Kart",
+    price: "43.99"
+}, {
+    name: "Sonic winter sports",
+    price: "29.99"
+}];
